110aab6f38a1e2610306651c94754bee2eabd7f1
alyonatokareva <99283237+alyonatokareva@users.noreply.github.com>
2022-06-04 16:55:42 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,14 +1,35 @@
 // Copyright 2021 NNTU-CS
-#include <string>
-#include <map>
-#include "tstack.h"
+#ifndef INCLUDE_TSTACK_H_
+#define INCLUDE_TSTACK_H_
 
-std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
-}
+template<typename T, int size>
+class TStack {
+private:
+     T arr[size];
+     int top;
+ public:
+     TStack() : top(-1) {}
+     
+     T get() {
+         if (isEmpty()) {
+         } else {
+             return arr[top];
+         }
+     }
+     bool isEmpty() const {
+         return top == -1;
+     }
+     bool isFull() const {
+         return top == size - 1;
+     }
 
-int eval(std::string pref) {
-  // добавьте код
-  return 0;
-}
+     void pop() {
+         if (top >= 0)
+             top--;
+     }
+     void push(T value) {
+         if (top < size - 1)
+             arr[++top] = value;
+     }
+};
+#endif  // INCLUDE_TSTACK_H_
